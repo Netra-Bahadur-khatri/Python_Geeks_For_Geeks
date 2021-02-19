34a530d7b225a1ce0038c9fefb48ef85b28634e1
Netra Kc <71728112+Netra-Bahadur-khatri@users.noreply.github.com>
2021-02-19 15:43:32 +0545

    Add files via upload

---
+++ GFG_05_Iteration.py
@@ -0,0 +1,35 @@
+
+# Iteration using an for and while loop in python in different data types
+
+
+# Ques no. 1
+# Python program to print an numbers from 1 to 10 using an while loop
+print("\n")
+i = 1
+print("Printing an numbers from 1 to 10 using an while loop in python: ")
+while i <= 10:
+    print(i,end=",")
+    i = i + 1
+
+print("\n\n")
+
+# Ques no. 2
+# Python program to print an strings by characters
+name = "Netra Bahadur Khatri"
+print("Printing an name by characters: ")
+for a in name:
+    print(a,end=", ")
+
+print("\n")
+
+# Ques no. 3
+# Python program to print an numbers from 0 to 10 using an for loop
+print("Printing an numbers from 0 to 10 using an range function: " )
+for a in range(0,10):
+    print(a, end=",")
+
+print("\n")
+
+
+
+
