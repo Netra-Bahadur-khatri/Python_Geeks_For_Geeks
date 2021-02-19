ef1cf6d35b068c0d9eb593feb3f6166667e331de
Netra Kc <71728112+Netra-Bahadur-khatri@users.noreply.github.com>
2021-02-19 15:43:59 +0545

    Add files via upload

---
+++ GFG_06_Sets.py
@@ -0,0 +1,21 @@
+
+
+"""
+Set is an undordered collection of data type that is iterable
+mutable and has no duplicate elements.
+"""
+# Creating an sets 
+string = "Hello, this is Netra kc"
+set1 = set(string)
+print(set1)
+
+a = 4
+b = 5
+set_add = set()
+set_add.add(a)
+set_add.add(b)
+print(set_add)
+
+# update() --> takes elements as list
+# remove() or discard() --> pass an elements to the function
+# pop() --> takes an
